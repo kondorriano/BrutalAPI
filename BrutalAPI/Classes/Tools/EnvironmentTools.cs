0c7a8eb03468c69d26cef89cacae616310d3bcb2
Nicolas Delgado <nicobito@gmail.com>
2024-05-27 19:31:36 +0200

    Added environment tools to be able to add combat environments?

---
+++ BrutalAPI/Classes/Tools/EnvironmentTools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace BrutalAPI
+{
+    public static class EnvironmentTools
+    {
+        public static void PrepareCombatEnvPrefab(string prefabBundlePath, string combatEnvID, AssetBundle fileBundle)
+        {
+            GameObject asset = fileBundle.LoadAsset<GameObject>(prefabBundlePath);
+            CombatEnvironmentHandler data = asset.GetComponent<CombatEnvironmentHandler>();
+            LoadedAssetsHandler.AddExternalCombatEnvironment(combatEnvID, data);
+        }
+    }
+}
